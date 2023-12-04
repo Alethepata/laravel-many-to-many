0705979558da3cd705ec2cc8e9da3492e6ba63c0
Alethepata <alessia.patane@gmail.com>
2023-12-04 15:55:50 +0100

    [Edit] $new_tecnology->name

--- database/seeders/TecnologiesTableSeeder.php
+++ database/seeders/TecnologiesTableSeeder.php
@@ -19,7 +19,7 @@ class TecnologiesTableSeeder extends Seeder
     {
         for($i = 0; $i < 5; $i++){
             $new_tecnology = new Tecnology();
-            $new_tecnology->name = $faker->words(2,true);
+            $new_tecnology->name = $faker->word();
             $new_tecnology->slug = Tecnology::generateSlug($new_tecnology->name);
             $new_tecnology->save();
         }
